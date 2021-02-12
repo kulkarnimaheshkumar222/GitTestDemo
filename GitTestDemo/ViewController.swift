34fe9122086f3b4d2d9d41b17fef5281857a4c1d
Mahesh Kulkarni <mahesh.kulkarni2biz4solutions.com>
2021-02-12 12:19:17 +0530

    Added viewController life cycle Methods

--- GitTestDemo/ViewController.swift
+++ GitTestDemo/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("In ViewDidLoad")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        print("In ViewWillAppear")
     }
 
 
